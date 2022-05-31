f760104da0391e514d3974dda5d60e94169bdad3
Lev Kurbanov <lev.k@smplt.net>
2022-05-31 20:28:42 +0500

    minor fix

--- src/MarketingBox.Email.Service/Modules/ClientModule.cs
+++ src/MarketingBox.Email.Service/Modules/ClientModule.cs
@@ -13,6 +13,10 @@ namespace MarketingBox.Email.Service.Modules
         protected override void Load(ContainerBuilder builder)
         {
             var serviceBusClient = builder.RegisterMyServiceBusTcpClient(Program.ReloadedSettings(e => e.MarketingBoxServiceBusHostPort), Program.LogFactory);
+            
+            builder.RegisterMyServiceBusSubscriberSingle<PasswordRecoveryEmailMessage>(
+                serviceBusClient, PasswordRecoveryEmailMessage.Topic, 
+                "MarketingBox-Email-Service-password-recovery", TopicQueueType.PermanentWithSingleConnection);
             builder.RegisterMyServiceBusSubscriberBatch<AffiliateUpdated>(
                 serviceBusClient, Topics.AffiliateUpdatedTopic, 
                 "MarketingBox-Email-Service", TopicQueueType.PermanentWithSingleConnection);
